a5824b96d1a0dc51737ce574d3ac19888309f92e
nsilva5 <nsilva5@uolinc.com>
2024-09-27 14:26:54 -0300

    coment: analise sobre as variaveis

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log('print, man');
+/*
+licao sobre variáveis:
+    - não utilize número para iniciar variáveis
+    - que os nomes de variáveis sejam únicas
+*/
 
 let name_i = 'Natan';
 console.log(name_i);
