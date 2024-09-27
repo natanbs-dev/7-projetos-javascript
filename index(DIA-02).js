ded1b713ef3b54bafc2552097fbc4b10cde578a1
nsilva5 <nsilva5@uolinc.com>
2024-09-27 17:44:45 -0300

    add: logicos, ternario e comparacao

--- index(DIA-02).js
+++ index(DIA-02).js
@@ -1,4 +1,6 @@
 /*
+operador aritmetico
+    somando + 1 ao valor
 let num1 = 5
 let num2 = 6
 let total = num1 * num2
@@ -7,5 +9,51 @@ console.log(total++)
 
 console.log(total)
 */
+
+/*
+operador de comparação
+
+let n1 = 3
+let n2 = 3
+
+//se é maior ou menor
+console.log(n1 + n2 <= 5)
+
+// se é igual ao valor da variável
+console.log(n1 === 5)
+
+// caso o valor não seja igual a
+console.log(n1 !== 3)
+*/
+
+/*
+OPERADORES TERNARIOS
+
+
+let drive = 90
+let speed = drive > 110 ? 'acima' : 'abaixo'
+console.log(speed)
+*/
+
+/*
+OPERADORES LÓGICOS
+    AND=&&
+    OR=!!
+    NOT=!
+*/
+
+let pessoa = {
+    name1: "Jonas",
+    temIdadeMinima: true,
+    temTitulo: true
+} 
+
+let podeVotar = pessoa.temIdadeMinima && pessoa.temTitulo;
+
+if (podeVotar === true) {
+    // let conteudo = 'me parece que ele pode votar de fato';
+    let conteudo = pessoa.name1 + ' pode votar, de fato';
+    console.log(conteudo)
+}
 
 
